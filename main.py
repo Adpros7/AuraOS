d764f86c2ef64ec857de1e2239e9f5297d20ed5c
Adpros7 <pranit.advik@gmail.com>
2025-11-09 20:19:49 -0500

    Add a read-only text widget with command input and output history.

--- main.py
+++ main.py
@@ -2,15 +2,40 @@ import tkinter as tk
 
 
 def main():
-    
-    def on_key(event):
-        print(f"Key pressed: {event.keysym}")
 
     root = tk.Tk()
     root.title("AuraOS")
     root.geometry("500x500")
-    root.bind("<Key>", on_key)
     root.configure(bg="#000000")  # Set the
+    text_box = tk.Text(root, bg="#000000", fg="#FFFFFF")
+    text_box.place(x=0, y=0, width=500, height=500)
+    text_box.insert("end", "Read-only: ", "ro")
+    text_box.insert("end", "you can type here\n")
+
+    # make only the "ro" tag read-only
+    def block_edit(event):
+        # if cursor inside read-only tag, block keypress
+        if "ro" in text_box.tag_names("insert"):
+            return "break"
+
+    def on_enter(event):
+        # insert text and make it read-only
+        line_start = text_box.index("insert linestart")
+        line_end = text_box.index("insert lineend")
+        typed_text = text_box.get(line_start, line_end)
+        text_box.insert(
+            "end",
+            f"\n{typed_text if not typed_text == "ls" or "file" else "wsggg"}",
+            "ro",
+        )
+        text_box.tag_add("ro", "end-3l", "end-1l")  # tag last 2 lines as read-only
+        text_box.tag_config("ro", foreground="gray")
+        return "break"  # optional: stop newline from being added
+
+    text_box.tag_add("ro", "1.0", "1.end")
+    text_box.tag_config("ro", foreground="gray")
+    text_box.bind("<Key>", lambda event: block_edit(event))
+    text_box.bind("<Return>", on_enter)
     root.mainloop()
 
 

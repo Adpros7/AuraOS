374473e1937915e89851196019b78b40fe494bad
Adpros7 <pranit.advik@gmail.com>
2025-11-09 19:54:13 -0500

    Convert CLI program to GUI application using tkinter.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from auraos!")
+import tkinter as tk
 
+def main():
+    root = tk.Tk()
+    root.title("AuraOS")
+    root.geometry("500x500")
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
